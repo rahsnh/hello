0fe3d7d4b2fa289050aa7ebc4736213536baca29
rahsnh <rahsnh@gmail.com>
2019-12-24 12:49:27 +0530

    Update flask_test.py

--- flask_test.py
+++ flask_test.py
@@ -3,10 +3,10 @@ from time import sleep
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/test")
 def hello():
 	sleep(10)
 	return {'body': 'hello'}
 
 if __name__ == '__main__':
-   app.run(host='127.0.0.1', port=6289)
+   app.run(threaded=True, port=5000)
